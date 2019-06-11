d0c114f35b54b55a538d6873617c51e13f79da1c
Diego Mendes <diego.mendes@amido.com>
2019-06-11 18:19:08 +0100

    Fixed image replacement script

--- src/services/api/xxAMIDOxx.xxSTACKSxx.API/dev-publish.sh
+++ src/services/api/xxAMIDOxx.xxSTACKSxx.API/dev-publish.sh
@@ -1,3 +1,5 @@
+GITROOT=`git rev-parse --show-toplevel`
+
 #build
 ./docker-build.sh
 
@@ -9,16 +11,26 @@ RELEASEDATE=`date --iso-8601=seconds`
 
 #kustomize - replace variables
 
+DEVFOLDER=$GITROOT/deploy/k8s/api/localhost
+
+rm -drf $DEVFOLDER  #clear temp folder before operation to avoid conflicts
+
 (
-	cd ../../../../deploy/k8s/api/base; 
+	cp -r $GITROOT/deploy/k8s/api/base $DEVFOLDER
+	cd $DEVFOLDER
 	
-	kustomize edit set image menuapi=amidostacksacrukstmp.azurecr.io/guibirow/xx-amido-xx.xx-stacks-xx.api:$TAG;
-	
+	#set image
+	kustomize edit set image manuapi-image=amidostacksacrukstmp.azurecr.io/guibirow/xx-amido-xx.xx-stacks-xx.api:$TAG;
+
+	#set annotations
 	kustomize edit add annotation version:$TAG;
 	kustomize edit add annotation app.kubernetes.io/version:$TAG;
 	kustomize edit add annotation release:0.0.1;
 	kustomize edit add annotation releasedOn:$RELEASEDATE;
+
+	#apply
+	kubectl apply -k .
 )
 
-#apply
-kubectl apply -k ../../../../deploy/k8s/api/base
+#clean temp dev folder
+rm -dr $DEVFOLDER
