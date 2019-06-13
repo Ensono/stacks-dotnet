e672214b76e66dac344eb52b310b0fa2410f0933
Diego Mendes <diego.mendes@amido.com>
2019-06-13 12:26:05 +0100

    Return only route method and path

--- src/services/api/xxAMIDOxx.xxSTACKSxx.API/Controllers/RoutesController.cs
+++ src/services/api/xxAMIDOxx.xxSTACKSxx.API/Controllers/RoutesController.cs
@@ -22,13 +22,22 @@ namespace test.Controllers
         public ActionResult Get()
         {
             var routes = _provider.ActionDescriptors.Items.Select(x => new { 
-                    Action = x.RouteValues["Action"], 
-                    Controller = x.RouteValues["Controller"], 
-                    Name = x.AttributeRouteInfo.Name, 
-                    Template = x.AttributeRouteInfo.Template 
-                }).ToList();
+                Action = x.RouteValues["Action"], 
+                Controller = x.RouteValues["Controller"], 
+                Name = x.AttributeRouteInfo.Name, 
+                Template = x.AttributeRouteInfo.Template 
+            }).ToList();
 
-            return Ok(routes);
+            return Ok(
+                routes
+                    .OrderBy(x => x.Controller)
+                    .ThenBy(x => x.Action)
+                    .ThenBy(x => x.Template)
+                    .Select(r => new {
+                        Method = r.Action.ToUpper(),
+                        Path = $"/{r.Template}"
+                    })
+                );
         }
     }
 }
