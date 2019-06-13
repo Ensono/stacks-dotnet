13f621ca6871adb6f1804a620238dbb3cfd0b152
Diego Mendes <diego.mendes@amido.com>
2019-06-13 10:33:41 +0100

    Added Ingress Refatored developer scripts for localbuild Updated default kustomization image

--- src/services/api/xxAMIDOxx.xxSTACKSxx.API/docker-build.sh
+++ src/services/api/xxAMIDOxx.xxSTACKSxx.API/docker-build.sh
@@ -1,1 +1,7 @@
-docker build -t xx-amido-xx.xx-stacks-xx.api .
+if [ $# -eq 0 ]; then TAG=`date -u "+%Y.%m.%d-%H%M"`; else TAG=$1; fi;
+
+IMAGE=`echo "${PWD##*/}" | tr '[:upper:]' '[:lower:]' | tr -d '\r'`
+
+echo "Building image $IMAGE:$TAG"
+
+docker build -t $IMAGE:$TAG .
--- src/services/api/xxAMIDOxx.xxSTACKSxx.API/docker-push.sh
+++ src/services/api/xxAMIDOxx.xxSTACKSxx.API/docker-push.sh
@@ -2,11 +2,23 @@
 
 REGISTRY="amidostacksacrukstmp.azurecr.io"
 REPO=`whoami`
-IMAGE="xx-amido-xx.xx-stacks-xx.api"
-TAG=`date -u "+%Y.%m.%d-%H%M"`
+#IMAGE="xx-amido-xx.xx-stacks-xx.api"
+#TAG=`date -u "+%Y.%m.%d-%H%M"`
+if [ $# -eq 2 ]; then 
+    IMAGE=$1; 
+    TAG=$2;
+elif [ $# -eq 0 ]; then 
+    IMAGE=`echo "${PWD##*/}" | tr '[:upper:]' '[:lower:]' | tr -d '\r'`; 
+    TAG=`date -u "+%Y.%m.%d-%H%M"`;
+else
+    echo "Invalid number of args. Use it without parameter to autogenerate values or pass image name and tag. i.e: '$(basename -- $0) imagename tag'"
+    exit 1
+fi;
 
 # Actions
 echo "Tagging image $IMAGE as $REGISTRY/$REPO/$IMAGE:$TAG"
+
+docker tag $IMAGE $REGISTRY/$REPO/$IMAGE:latest
 docker tag $IMAGE $REGISTRY/$REPO/$IMAGE:$TAG
 
 echo ""
