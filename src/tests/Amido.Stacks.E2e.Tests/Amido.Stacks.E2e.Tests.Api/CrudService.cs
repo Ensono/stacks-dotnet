856009f195abec5537e4f442e13791f80fa3335d
Jack Middleton <Jack.Middleton@amido.com>
2019-06-14 14:01:56 +0100

    Added basic BDDfy test using a httpclient

---
+++ src/tests/Amido.Stacks.E2e.Tests/Amido.Stacks.E2e.Tests.Api/CrudService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amido.Stacks.E2e.Tests.Api
+{
+    public class CrudService
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public CrudService(string baseUri)
+        {
+            _httpClient.BaseAddress = new Uri(baseUri);
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+        }
+
+        public async Task<HttpResponseMessage> Get(string path)
+        {
+            return await _httpClient.GetAsync(path);
+        }
+    }
+}
---
+++ src/tests/Amido.Stacks.E2e.Tests/Amido.Stacks.E2e.Tests.Api/GetHealthStory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace Amido.Stacks.E2e.Tests.Api
+{
+    [Story(
+        AsA = "Test engineer starting a new project",
+        IWant = "A test automation template",
+        SoThat = "I can reduce sprint 0 duration")]
+    public class GetHealth
+    {
+        private CrudService _service;
+        private HttpResponseMessage _response;
+
+        void GivenIHaveATestEndpoint()
+        {
+            _service = new CrudService("http://52.151.102.150");
+        }
+
+        async Task WhenIMakeAGetRequestToTheEndpoint()
+        {
+            _response = await _service.Get("/api/menu/health");
+        }
+
+        void ThenTheResultShouldIncludeAListOfEndpoints()
+        {
+            Assert.True(_response.IsSuccessStatusCode);
+            Assert.Equal("ok", _response.Content.ReadAsStringAsync().Result);
+        }
+
+        [Fact]
+        public void HealthShouldBeOK()
+        {
+            this.BDDfy();
+        }
+    }
+}
