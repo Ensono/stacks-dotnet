cd1d87df83d4afe18d910fbaf9025553c3ddac98
Jack Middleton <Jack.Middleton@amido.com>
2019-07-10 08:43:24 +0100

    Added CreateOrUpdateCategory model and builder

---
+++ src/tests/xxAMIDOxx.xxSTACKSxx.Tests/xxAMIDOxx.xxSTACKSxx.Tests.Api/Builders/CreateOrUpdateCategoryBuilder.cs
@@ -0,0 +1,31 @@
+﻿using xxAMIDOxx.xxSTACKSxx.Tests.Api.Models;
+
+namespace xxAMIDOxx.xxSTACKSxx.Tests.Api.Builders
+{
+    public class CreateOrUpdateCategoryBuilder : IBuilder<CreateOrUpdateCategory>
+    {
+        private CreateOrUpdateCategory model;
+
+        public CreateOrUpdateCategoryBuilder()
+        {
+            model = new CreateOrUpdateCategory();
+        }
+
+        public CreateOrUpdateCategoryBuilder WithName(string name)
+        {
+            model.name = name;
+            return this;
+        }
+
+        public CreateOrUpdateCategoryBuilder WithDescription(string description)
+        {
+            model.description = description;
+            return this;
+        }
+
+        public CreateOrUpdateCategory Build()
+        {
+            return model;
+        }
+    }
+}
---
+++ src/tests/xxAMIDOxx.xxSTACKSxx.Tests/xxAMIDOxx.xxSTACKSxx.Tests.Api/Models/CreateOrUpdateCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace xxAMIDOxx.xxSTACKSxx.Tests.Api.Models
+{
+    public class CreateOrUpdateCategory
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
