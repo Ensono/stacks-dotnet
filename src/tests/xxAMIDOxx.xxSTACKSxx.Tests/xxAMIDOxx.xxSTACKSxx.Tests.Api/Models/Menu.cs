19db0135598c6d0bb0147f64bb038a5492d1de03
Jack Middleton <Jack.Middleton@amido.com>
2019-07-10 08:24:12 +0100

    Updated menu model and builder to match new API definitions

--- src/tests/xxAMIDOxx.xxSTACKSxx.Tests/xxAMIDOxx.xxSTACKSxx.Tests.Api/Models/Menu.cs
+++ src/tests/xxAMIDOxx.xxSTACKSxx.Tests/xxAMIDOxx.xxSTACKSxx.Tests.Api/Models/Menu.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace xxAMIDOxx.xxSTACKSxx.Tests.Api.Models
+﻿namespace xxAMIDOxx.xxSTACKSxx.Tests.Api.Models
 {
     public class Menu
     {
-        public string id { get; set; }
         public string name { get; set; }
-        public List<Category> categories { get; set; }
+        public string description { get; set; }
         public bool enabled { get; set; }
     }
 }
