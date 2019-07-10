a2f336dbf118f7fba59b470ec483259caef04a6c
Jack Middleton <Jack.Middleton@amido.com>
2019-07-10 09:50:12 +0100

    Added search result models

---
+++ src/tests/xxAMIDOxx.xxSTACKSxx.Tests/xxAMIDOxx.xxSTACKSxx.Tests.Api/Models/SearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace xxAMIDOxx.xxSTACKSxx.Tests.Api.Models
+{
+    public class SearchResult
+    {
+        public int size { get; set; }
+        public int offset { get; set; }
+        public List<SearchResultItem> results { get; set; }
+    }
+}
