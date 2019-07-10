7f4bd44469043ce41adf461033b1532eef4eb3ba
Jack Middleton <Jack.Middleton@amido.com>
2019-07-10 09:50:12 +0100

    Added search result models

---
+++ src/tests/xxAMIDOxx.xxSTACKSxx.Tests/xxAMIDOxx.xxSTACKSxx.Tests.Api/Models/SearchResultItem.cs
@@ -0,0 +1,10 @@
+﻿namespace xxAMIDOxx.xxSTACKSxx.Tests.Api.Models
+{
+    public class SearchResultItem
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public bool enabled { get; set; }
+    }
+}
