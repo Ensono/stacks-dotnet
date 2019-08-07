c64e7b1968b03d6243329e21cc4f056ef8c2a56f
William Ayerst <william.ayerst@amido.com>
2019-08-07 11:13:02 +0100

    add

--- deploy/terraform/azure/vars.tf
+++ deploy/terraform/azure/vars.tf
@@ -14,6 +14,10 @@ variable "name_environment" {
 variable "name_platform" {
 }
 
+variable "name_component" {
+  
+}
+
 # Each region must have corresponding a shortend name for resource naming purposes 
 variable "location_name_map" {
   type = map(string)
@@ -55,9 +59,9 @@ variable "cosmosdb_failover_location" {
 ###########################
 
 locals {
-  cosmosdb_account_name            = "${var.name_company}${var.name_platform}cdb${var.location_name_map[var.resource_group_location_env]}${var.name_environment}"
-  cosmosdb_table_name             = "${var.name_company}${var.name_platform}tbl${var.location_name_map[var.resource_group_location_env]}${var.name_environment}"
-  resource_group_name_env      = "${var.name_company}-${var.name_platform}-rg-${var.location_name_map[var.resource_group_location_env]}-${var.name_environment}"
+  cosmosdb_account_name            = "${var.name_company}${var.name_platform}${var.name_component}cdb${var.location_name_map[var.resource_group_location_env]}${var.name_environment}"
+  cosmosdb_table_name             = "${var.name_company}${var.name_platform}${var.name_component}tbl${var.location_name_map[var.resource_group_location_env]}${var.name_environment}"
+  resource_group_name_env      = "${var.name_company}-${var.name_platform}-${var.name_component}-rg-${var.location_name_map[var.resource_group_location_env]}-${var.name_environment}"
 
 }
 
