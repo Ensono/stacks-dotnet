10cfa6b179d2ce9ac9e63cb44fa4e503f31b3e5e
William A <11176822+williamayerst@users.noreply.github.com>
2019-08-07 09:27:00 +0100

    452 - Release Note Generation (#28)
    * Run release note creation step, retrieve URL's and update global var names
    
    * updated package version to master branch
    
    * cosmosDB terraform scaffolding
    
    * fixed path to get-azstorage-url.sh
    
    * Bug/fix build csharp 7 2 features (#30)
    
    * Added <LangVersion>7.2</LangVersion> to packages project
    
    * added menu credentials group and setting key value
    
    * absolute paths on echo
    
    * fixed secret path
    
    * refactor to generic
    
    * scoping variable group
    
    * Added config for test environment
    
    * only running on master

--- deploy/terraform/azure/main.tf
+++ deploy/terraform/azure/main.tf
@@ -1,74 +1,44 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "${local.resource_group_name}"
-  location = "${var.resource_group_location}"
-  tags     = "${var.resource_group_tags}"
+##################################################
+# ResourceGroups
+
+resource "azurerm_resource_group" "env" {
+  name     = local.resource_group_name_env
+  location = var.resource_group_location_env
+  tags     = var.resource_group_tags
 }
 
-/* resource "azurerm_traffic_manager_profile" "traffic_manager" {
-  name                = "${local.traffic_manager_name}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+##################################################
+# CosmosDB Resource
 
-  traffic_routing_method = "Weighted"
-
-  dns_config {
-    relative_name = "${local.traffic_manager_name}"
-    ttl           = 60
-  }
-
-  monitor_config {
-    protocol = "http"
-    port     = 80
-    path     = "/"
-  }
-
-  tags = "${var.resource_group_tags}"
+resource "azurerm_cosmosdb_table" "table" {
+  name                = local.cosmosdb_table_name
+  resource_group_name = azurerm_cosmosdb_account.db.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.db.name
 }
 
-resource "azurerm_traffic_manager_endpoint" "endpoints" {
-  count               = "${length(var.cluster_ingress_address)}"
-  name                = "${element(keys(var.cluster_ingress_address), count.index)}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  profile_name        = "${azurerm_traffic_manager_profile.traffic_manager.name}"
-  target              = "${var.name_company}-${var.name_platform}-${var.name_project}-${var.name_environment}-${element(keys(var.cluster_ingress_address), count.index)}.${var.dns_zone}"
-  type                = "externalEndpoints"
-  weight              = 100
-}
 
-resource "azurerm_dns_cname_record" "ingress_cname_record" {
-  count               = "${length(var.cluster_ingress_address)}"
-  name                = "${var.name_company}-${var.name_platform}-${var.name_project}-${var.name_environment}-${element(keys(var.cluster_ingress_address), count.index)}"
-  zone_name           = "${var.dns_zone}"
-  resource_group_name = "${var.dns_zone_resource_group_name}"
-  ttl                 = "${var.dns_record_ttl_seconds}"
-  record = "${var.cluster_ingress_address[element(keys(var.cluster_ingress_address), count.index)]}"
-}
-
-resource "azurerm_dns_cname_record" "tm_cname_record" {
-  name                = "${local.dns_record_address_tm}"
-  zone_name           = "${var.dns_zone}"
-  resource_group_name = "${var.dns_zone_resource_group_name}"
-  ttl                 = "${var.dns_record_ttl_seconds}"
-  record              = "${azurerm_traffic_manager_profile.traffic_manager.fqdn}"
-}
-*/
-
-resource "azurerm_cosmosdb_account" "cosmosdb" {
-  name                = "${local.cosmosdb_account_name}"
-  location            = "${var.resource_group_location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+resource "azurerm_cosmosdb_account" "db" {
+  name                = local.cosmosdb_account_name
+  location            = azurerm_resource_group.env.location
+  resource_group_name = azurerm_resource_group.env.name
   offer_type          = "Standard"
-  kind                = "MongoDB"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover = true
 
   consistency_policy {
-    consistency_level = "Session"
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
   }
 
-
-  failover_policy {
-    location = "North Europe"
-    priority = 0
+  geo_location {
+    location          = azurerm_resource_group.env.location
+    failover_priority = 0
   }
 
-  tags = "${var.resource_group_tags}"
-  
+  geo_location {
+    location          = var.cosmosdb_failover_location
+    failover_priority = 1
+  }
 }
