dee7f511432ef417c5fae78119285a6a7559082a
Jack Middleton <Jack.Middleton@amido.com>
2019-08-13 16:35:23 +0100

    Added Create Menu test

---
+++ src/tests/xxAMIDOxx.xxSTACKSxx.Performance/xxAMIDOxx.xxSTACKSxx.Gatling.Tests/src/test/scala/xxAMIDOxx/xxSTACKSxx/Gatling/Tests/requests/CreateMenuRequest.scala
@@ -0,0 +1,14 @@
+package xxAMIDOxx.xxSTACKSxx.Gatling.Tests.requests
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+import xxAMIDOxx.xxSTACKSxx.Gatling.Tests.config.Config.app_url
+
+object CreateMenuRequest {
+  var create_menu: HttpRequestBuilder = http("Create Menu")
+    .post(app_url + "/v1/menu")
+    .body(RawFileBody("./src/test/resources/bodies/CreateMenu.json")).asJson
+    .header("content-type", "application/json")
+    .check(status is 201)
+}
---
+++ src/tests/xxAMIDOxx.xxSTACKSxx.Performance/xxAMIDOxx.xxSTACKSxx.Gatling.Tests/src/test/scala/xxAMIDOxx/xxSTACKSxx/Gatling/Tests/simulations/CreateMenuSimulation.scala
@@ -0,0 +1,13 @@
+package xxAMIDOxx.xxSTACKSxx.Gatling.Tests.simulations
+
+import io.gatling.core.Predef._
+import scala.concurrent.duration._
+import xxAMIDOxx.xxSTACKSxx.Gatling.Tests.config.Config.users
+import xxAMIDOxx.xxSTACKSxx.Gatling.Tests.scenarios.CreateMenuScenario
+
+class CreateMenuSimulation extends Simulation {
+  private val createMenuRampExec = CreateMenuScenario.createMenuScenario
+    .inject(rampUsers(users) during(10 seconds))
+
+  setUp(createMenuRampExec)
+}
