60b616d1e39b0f8ccef7e7e4ec44a1fa157fa69b
Kleber Guedes <56262054+kleber-amido@users.noreply.github.com>
2021-07-14 15:36:24 +0100

    Re-estabilishing src folder in the root, so build pipeline works while we wait for the DevOps changes

---
+++ src/tests/Functional/xxAMIDOxx.xxSTACKSxx.API.FunctionalTests/Configuration/ConfigModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xxAMIDOxx.xxSTACKSxx.API.FunctionalTests.Configuration
+{
+    public class ConfigModel
+    {
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ src/tests/Functional/xxAMIDOxx.xxSTACKSxx.API.FunctionalTests/Models/MenuRequest.cs
@@ -0,0 +1,9 @@
+namespace xxAMIDOxx.xxSTACKSxx.API.FunctionalTests.Models
+{
+    public class MenuRequest
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public bool enabled { get; set; }
+    }
+}
