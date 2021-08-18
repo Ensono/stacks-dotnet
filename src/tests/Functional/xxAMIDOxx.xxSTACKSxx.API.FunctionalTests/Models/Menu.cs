f05d02fba238437b3c6a724985344fac1c142ee2
Kleber Guedes <56262054+kleber-amido@users.noreply.github.com>
2021-08-18 10:48:31 +0100

    Adding Functional tests to solution (#154)

---
+++ src/tests/Functional/xxAMIDOxx.xxSTACKSxx.API.FunctionalTests/Models/Menu.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace xxAMIDOxx.xxSTACKSxx.API.FunctionalTests.Models
+{
+    public class Menu
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public List<Category> categories { get; set; }
+        public bool enabled { get; set; }
+    }
+}
