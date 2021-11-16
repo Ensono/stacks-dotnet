83a7bb73aff72818baa46778be8098849529c8e4
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-11-16 13:54:38 +0000

    Debugging
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- build/scripts/functions/Connect-Azure.ps1
+++ build/scripts/functions/Connect-Azure.ps1
@@ -43,8 +43,6 @@ function Connect-Azure() {
         }
     }
 
-    dir env:
-
     # check that the missing vars is empty
     if ($missing.Count -gt 0) {
         Write-Error -Message ("Missing required values: {0}" -f ($missing -Join ", "))
