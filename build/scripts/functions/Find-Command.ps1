689425d577a98d2558d782f8a88997734066f5aa
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-11-16 09:24:18 +0000

    Updated scripts to use the correct path to tools
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- build/scripts/functions/Find-Command.ps1
+++ build/scripts/functions/Find-Command.ps1
@@ -12,8 +12,10 @@ function Find-Command {
     # Find the path to the named command
     $command = Get-Command -Name $Name -ErrorAction SilentlyContinue
     if ([string]::IsNullOrEmpty($command)) {
-        Write-Error -Message ("'{0}' command cannot be found, please ensure it is installed" -f $Name)
+        Write-Error -Message ("'{0}' command cannot be found in the path, please ensure it is installed" -f $Name)
         exit 1
+    } else {
+        Write-Host ("Tool found: {0}" -f $command.Source)
     }
 
     return $command
