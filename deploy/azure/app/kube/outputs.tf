12017818ca42a98eff28d85d4d6d5255346fa2c0
wayerst <william.ayerst@amido.com>
2022-03-10 09:49:39 +0000

    Depreciates missing vars

--- deploy/azure/app/kube/outputs.tf
+++ deploy/azure/app/kube/outputs.tf
@@ -1,3 +1,4 @@
+# CosmosDB Configuration
 output "cosmosdb_database_name" {
   description = "CosmosDB Database name"
   value       = module.app.cosmosdb_database_name
@@ -19,6 +20,7 @@ output "cosmosdb_primary_master_key" {
   value       = module.app.cosmosdb_primary_master_key
 }
 
+# Redis Configuration
 output "redis_cache_key" {
   description = "Primary Key for accessing the RedisCache, should only be used in applications running outside of AzureCloud"
   sensitive   = true
@@ -30,6 +32,7 @@ output "redis_cache_hostname" {
   value       = module.app.redis_cache_hostname
 }
 
+# Azure Configuration
 output "resource_group" {
   description = "Resource group name for the app"
   value       = module.app.resource_group
@@ -40,8 +43,7 @@ output "dns_name" {
   value       = module.app.dns_name
 }
 
-
-
+# Core State Query Outputs (to reduce variable duplication)
 output "aks_resource_group_name" {
   description = "Name of the Resource Group in which the K8s cluster is deployed"
   value       = data.terraform_remote_state.core.outputs.aks_resource_group_name
@@ -70,6 +72,12 @@ output "acr_registry_name" {
 output "app_insights_name" {
   description = "Name of the Application Insights instance"
   value       = data.terraform_remote_state.core.outputs.app_insights_name
+}
+
+output "app_insights_instrumentation_key" {
+  description = "App Insights key for downstream deploymnent use"
+  value       = data.azurerm_application_insights.example.instrumentation_key
+  sensitive   = true
 }
 
 output "app_gateway_ip" {
