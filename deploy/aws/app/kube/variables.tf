163d18ec2366af5bb68d18f025b399872ad2462e
William Ayerst <11176822+williamayerst@users.noreply.github.com>
2023-09-05 14:49:27 +0100

    Amends tags

--- deploy/aws/app/kube/variables.tf
+++ deploy/aws/app/kube/variables.tf
@@ -42,13 +42,14 @@ locals {
   # This can be extended like adding cost-code or organization name.
   default_tags = {
     Environment = var.name_environment
+    Created_by  = "stacks"
+    Owner       = "stacks"
+    Owner_email = "stacks@ensono.com"
+    Team        = "Platform Engineering"
     Component   = var.name_component
     Project     = var.name_project
     Company     = var.name_company
     Region      = var.region
   }
-}
-
-locals {
   account_id = data.aws_caller_identity.current.account_id
 }
