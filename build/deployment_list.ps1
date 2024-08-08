4b8060e2e027f9e8b13fc7c45906e563a61642f1
Saul France <saul.france@ensono.com>
2024-08-08 09:31:51 +0100

    feat: update helm chart to support aws

--- build/deployment_list.ps1
+++ build/deployment_list.ps1
@@ -1,25 +1,8 @@
 @(
-    @{
-      displayName = "AwsDeployment"
-      template = "deploy/k8s/aws/base_api-deploy.yml"
-      vars = @{
-          dns_pointer = "`${ENV_NAME}-`${DOMAIN}.`${DNS_BASE_DOMAIN}"
-          environment = "`${ENV_NAME}"
-          tls_domain = "`${DNS_BASE_DOMAIN}"
-          k8s_app_route = "`${K8S_APP_ROUTE}"
-          log_level = "Debug"
-          k8s_image = "`${DOCKER_REGISTRY}/`${DOCKER_IMAGE_NAME}:`${DOCKER_IMAGE_TAG}"
-          cloudwatch_log_group_name = "`${CLOUDWATCH_LOG_GROUP}"
-          cloudwatch_log_prefix ="`${CLOUDWATCH_STREAM_PREFIX}"
-          cloudwatch_region = "`${REGION}"
-          version = "`${DOCKER_IMAGE_TAG}"
-          rewrite_target = '/$([char]0x0024)2' # Using UniCode to prevent substitution
-      }
-  }
 
   @{
-    displayName = "AzureHelm"
-    template = "deploy/helm/stacks-dotnet/values.yaml"
+    displayName = "Helm"
+    template = "deploy/helm/stacks-dotnet/base_values.yaml"
     vars = @{
         host = "`${ENV_NAME}-`${DOMAIN}.`${DNS_BASE_DOMAIN}"
         environment = "`${ENV_NAME}"
@@ -28,6 +11,8 @@
         k8s_image = "`${DOCKER_REGISTRY}/`${DOCKER_IMAGE_NAME}:`${DOCKER_IMAGE_TAG}"
         aadpodidentitybinding = "stacks-webapp-identity"
         app_insights_key = "`${APP_INSIGHTS_INSTRUMENTATION_KEY}"
+        cloudwatch_log_group_name = "`${CLOUDWATCH_LOG_GROUP}"
+        cloudwatch_log_prefix ="`${CLOUDWATCH_STREAM_PREFIX}"
         version = "`${DOCKER_IMAGE_TAG}"
         rewrite_target = '/$([char]0x0024)2' # Using UniCode to prevent substitution
     }
