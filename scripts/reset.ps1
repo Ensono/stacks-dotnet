b8470f4d48c8417ee79233001d1518ac29ffec03
Chris Butler <christopher_g_butler@hotmail.co.uk>
2024-08-14 09:40:03 +0100

    fixing ServiceBus link failure

--- scripts/reset.ps1
+++ scripts/reset.ps1
@@ -2,3 +2,6 @@
 
 rm * -force -recurse; dotnet new stacks-webapi --name SimpleApi; code .
 start devenv .\SimpleApi\simple-api\src\api\SimpleApi.API.sln
+
+rm * -force -recurse; dotnet new stacks-az-func-cosmosdb-worker --name MyCode; code .
+start devenv .\MyCode\func-cosmosdb-worker\src\functions\MyCode.Worker.sln
