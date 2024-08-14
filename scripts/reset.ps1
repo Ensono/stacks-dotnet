d017ede6cea93c65da34f371bc278256e2fe3351
Chris Butler <christopher_g_butler@hotmail.co.uk>
2024-08-14 12:33:51 +0100

    moving template to proj level level

--- scripts/reset.ps1
+++ scripts/reset.ps1
@@ -8,3 +8,12 @@ start devenv .\MyCode\func-cosmosdb-worker\src\functions\MyCode.Worker.sln
 
 rm * -force -recurse; dotnet new stacks-az-func-aeh-listener --name MyCode --domain Dom; code .
 start devenv .\MyCode\func-aeh-listener\src\functions\MyCode.Listener.sln
+
+rm * -force -recurse; dotnet new stacks-az-func-asb-listener --name MyCode --domain Dom; code .
+start devenv .\MyCode\func-asb-listener\src\functions\MyCode.Listener.sln
+
+rm * -force -recurse; dotnet new stacks-asb-worker --name MyCode --domain Dom; code .
+start devenv .\MyCode\background-worker\src\worker\MyCode.BackgroundWorker.sln
+
+rm * -force -recurse; dotnet new stacks-cqrs-app --name MyCode --domain Dom; code .
+start devenv .\MyCode\cqrs\src\api\MyCode.API.sln
