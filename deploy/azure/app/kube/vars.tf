e76e4e7c8ac79e1ad79cbf09ab76cb71b32cae92
Saul France <saul.france@ensono.com>
2024-08-22 17:00:57 +0100

    feat: Update build, deploy and taskctl to support mono-repo (#433)
    * feat: update pipelines for monorepo
    
    * fix: remove duplicate tf variable
    
    * fix: update build path
    
    * chore: remove tests
    
    * fix: add path to tests
    
    * fix: add path to tests
    
    * fix: remove path from tests
    
    * fix: update test path and downgrade coverlet
    
    * fix: update coverage path
    
    * chore: update azure pipeline path
    
    * fix: path
    
    * fix: change directory
    
    * fix: change directory
    
    * fix: update path
    
    * fix: update path
    
    * fix: update functional tests path
    
    * fix: update self_repo_src
    
    * fix: update taskctl build container
    
    * fix: arg
    
    * fix: coverage target
    
    * fix: update tf state key
    
    * fix: update service port
    
    * fix: revert azure terraform changes
    
    * chore: remove terraform version check
    
    * chore: add comment to remove workflow dispatch
    
    * chore: temp remove component tests
    
    * fix: update cqrs sonar project name
    
    * fix: update cqrs dockerfile
    
    * chore: run component tests
    
    * chore: use domain for test report name
    
    * chore: remove workflow dispatch trigger
    
    * chore: use domain for test report name
    
    * chore: update templates

---
+++ deploy/azure/app/kube/vars.tf
@@ -0,0 +1,153 @@
+############################################
+# AUTHENTICATION
+############################################
+# RELYING PURELY ON ENVIRONMENT VARIABLES as the user can control these from their own environment
+############################################
+# NAMING
+############################################
+
+variable "name_company" {
+  type = string
+}
+
+variable "name_project" {
+  type = string
+}
+
+variable "name_component" {
+  type = string
+}
+
+variable "name_domain" {
+  type = string
+}
+
+variable "stage" {
+  type = string
+}
+
+variable "attributes" {
+  default = []
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+# Each region must have corresponding a shortend name for resource naming purposes
+variable "location_name_map" {
+  type = map(string)
+
+  default = {
+    northeurope   = "eun"
+    westeurope    = "euw"
+    uksouth       = "uks"
+    ukwest        = "ukw"
+    eastus        = "use"
+    eastus2       = "use2"
+    westus        = "usw"
+    eastasia      = "ase"
+    southeastasia = "asse"
+  }
+}
+
+############################################
+# AZURE INFORMATION
+############################################
+
+variable "resource_group_location" {
+  type = string
+}
+
+variable "dns_record" {
+  description = ""
+  type        = string
+}
+
+###########################
+# CONDITIONAL SETTINGS
+##########################
+variable "create_cosmosdb" {
+  description = "Whether to create a cosmosdb or not for this application"
+  type        = bool
+}
+
+variable "create_cache" {
+  type        = bool
+  description = "Whether to create a RedisCache"
+}
+
+variable "create_dns_record" {
+  type = bool
+}
+
+variable "create_cdn_endpoint" {
+  type = bool
+}
+
+variable "app_bus_type" {
+  type    = string
+  default = "servicebus"
+}
+
+###########################
+# CosmosDB SETTINGS
+##########################
+variable "cosmosdb_sql_container" {
+  type        = string
+  description = "Specify the SQLContainer name in CosmosDB"
+}
+
+variable "cosmosdb_sql_container_partition_key" {
+  type        = string
+  description = "Specify partition key"
+}
+
+variable "cosmosdb_kind" {
+  type        = string
+  description = "Specify the CosmosDB kind"
+}
+variable "cosmosdb_offer_type" {
+  type        = string
+  description = "Specify the offer type"
+}
+
+###########################
+# Core infrastructure settings
+##########################
+variable "core_environment" {
+  type        = string
+  description = "Name of the environment for the core infrastructure"
+  default     = "nonprod"
+}
+
+variable "tfstate_key" {
+  type        = string
+  description = "Name of the key in remote storage for the core environmnent"
+}
+
+variable "tfstate_storage_account" {
+  type        = string
+  description = "Name of the storage account that holds the Terraform state"
+}
+
+variable "tfstate_container_name" {
+  type        = string
+  description = "Name of the container in the specified storage account holding the state"
+}
+
+variable "tfstate_resource_group_name" {
+  type        = string
+  description = "Name of the resource group that holds the the above resources"
+}
+
+##########################
+# App Insights Lookup
+##########################
+variable "app_insights_name" {
+  type        = string
+  default     = ""
+  description = "app insights name for key retriaval in memory"
+}
+
