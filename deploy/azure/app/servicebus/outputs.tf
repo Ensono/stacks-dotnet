d6e0edf042ce46a5727943efaa4f0ed65b1801c6
Saul France <saul.france@ensono.com>
2024-08-30 09:45:06 +0100

    feat: update azure pipelines for the mono-repo (#469)
    * fix: update functional test directory
    
    * fix: conditional outputs for app bus type
    
    * feat: update helm for sns and dynamodb
    
    * chore: add workflow dispatch
    
    * fix: helm conditions
    
    * chore: add workflow dispatch
    
    * feat: update helm for azure cqrs
    
    * fix: if statements for empty values
    
    * feat: add pipeline for cqrs
    
    * fix: linting
    
    * fix: update cqrs variable group
    
    * fix: update cqrs vars
    
    * chore: separate taskctl steps
    
    * fix: linting
    
    * fix: update cqrs vars
    
    * chore: comment out service bus function
    
    * chore: publish helm values
    
    * fix: publish helm values
    
    * fix: helm cosmosdb key
    
    * feat: publish functional test report
    
    * chore: update api pipeline
    
    * chore: update variables
    
    * feat: update azure templates
    
    * chore: update stacks cli replacements
    
    * fix: linting
    
    * chore: move nuget pipelines

--- deploy/azure/app/servicebus/outputs.tf
+++ deploy/azure/app/servicebus/outputs.tf
@@ -33,18 +33,18 @@ output "servicebus_subscription_filtered_id" {
   value       = azurerm_servicebus_subscription.sb_sub_2.id
 }
 
-output "function_publisher_id" {
-  value = azurerm_function_app.function_publisher.id
-}
+# output "function_publisher_id" {
+#   value = azurerm_function_app.function_publisher.id
+# }
 
-output "function_listener_id" {
-  value = azurerm_function_app.function_listener.id
-}
+# output "function_listener_id" {
+#   value = azurerm_function_app.function_listener.id
+# }
 
-output "function_publisher_name" {
-  value = azurerm_function_app.function_publisher.name
-}
+# output "function_publisher_name" {
+#   value = azurerm_function_app.function_publisher.name
+# }
 
-output "function_listener_name" {
-  value = azurerm_function_app.function_listener.name
-}
+# output "function_listener_name" {
+#   value = azurerm_function_app.function_listener.name
+# }
