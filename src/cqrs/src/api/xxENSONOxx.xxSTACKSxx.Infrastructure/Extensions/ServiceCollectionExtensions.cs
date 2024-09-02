9cb400968881f64298a3be7e4d18f3bfa08b7c21
Sharon Russell <sharon-russell@outlook.com>
2024-09-02 17:10:22 +0100

    refactor: 7711 removal of SNS package (#471)
    * refactor: minor tidyup of SNS
    
    * refactor: 7711 - remove the SNS package
    
    * refactor: 7711 remove references to deleted csproj
    
    * refactor: update template
    
    * refactor: update template
    
    * refactor: PR review comments
    
    * refactor: also change this one
    
    * refactor: 7711 add the precompilation condition
    
    * refactor: try this way instead
    
    * refactor: also add conditions around the code

---
+++ src/cqrs/src/api/xxENSONOxx.xxSTACKSxx.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+#if (EventPublisherAwsSns)
+
+using Amazon.SimpleNotificationService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using xxENSONOxx.xxSTACKSxx.Infrastructure.Publishers;
+using xxENSONOxx.xxSTACKSxx.Shared.Application.CQRS.ApplicationEvents;
+
+namespace xxENSONOxx.xxSTACKSxx.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Add the AWS SNS client for IEventConsumer and IApplicationEventPublisher
+    /// </summary>
+    public static IServiceCollection AddAwsSns(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDefaultAWSOptions(configuration.GetAWSOptions());
+        services.AddAWSService<IAmazonSimpleNotificationService>();
+        services.AddTransient<IApplicationEventPublisher, EventPublisher>();
+
+        return services;
+    }
+}
+
+#endif
