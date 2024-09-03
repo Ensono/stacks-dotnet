b0b9284d0e082912edabf637796d82f3e3cc459a
Sharon Russell <sharon.russell@ensono.com>
2024-09-03 16:51:45 +0100

    refactor: remove unused parts of project and update templating

--- src/cqrs/src/api/xxENSONOxx.xxSTACKSxx.Infrastructure/Publishers/EventHubEventPublisher.cs
+++ src/cqrs/src/api/xxENSONOxx.xxSTACKSxx.Infrastructure/Publishers/EventHubEventPublisher.cs
@@ -1,5 +1,3 @@
-#if (EventPublisherEventHub)
-
 using System;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,32 +7,30 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using xxENSONOxx.xxSTACKSxx.Shared.Application.CQRS.ApplicationEvents;
 
-namespace xxENSONOxx.xxSTACKSxx.Infrastructure.Publishers
+namespace xxENSONOxx.xxSTACKSxx.Infrastructure.Publishers;
+
+public class EventHubEventPublisher(
+    ILogger<EventHubEventPublisher> log,
+    EventHubProducerClient eventHubProducerClient)
+    : IApplicationEventPublisher
 {
-    public class EventHubEventPublisher(
-        ILogger<EventHubEventPublisher> log,
-        EventHubProducerClient eventHubProducerClient)
-        : IApplicationEventPublisher
+    public async Task PublishAsync(IApplicationEvent applicationEvent)
     {
-        public async Task PublishAsync(IApplicationEvent applicationEvent)
+        log.LogInformation($"Publishing event {applicationEvent.CorrelationId}");
+
+        EventDataBatch eventDataBatch = await eventHubProducerClient.CreateBatchAsync();
+
+        var eventReading = JsonConvert.SerializeObject(applicationEvent);
+        eventDataBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(eventReading)));
+
+        try
         {
-            log.LogInformation($"Publishing event {applicationEvent.CorrelationId}");
-
-            EventDataBatch eventDataBatch = await eventHubProducerClient.CreateBatchAsync();
-
-            var eventReading = JsonConvert.SerializeObject(applicationEvent);
-            eventDataBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(eventReading)));
-
-            try
-            {
-                await eventHubProducerClient.SendAsync(eventDataBatch);
-                log.LogInformation($"Event {applicationEvent.CorrelationId} has been published.");
-            }
-            catch (Exception exception)
-            {
-                log.LogError($"Something went wrong. Exception thrown: {exception.Message}");
-            }
+            await eventHubProducerClient.SendAsync(eventDataBatch);
+            log.LogInformation($"Event {applicationEvent.CorrelationId} has been published.");
+        }
+        catch (Exception exception)
+        {
+            log.LogError($"Something went wrong. Exception thrown: {exception.Message}");
         }
     }
 }
-#endif
