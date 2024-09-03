ae67b5b4aade675d0e86cb4e8a763c0498b97406
Ali Russell <awjrussell@live.co.uk>
2024-09-03 16:02:01 +0100

    refactor: Update OpenTelemetry instrumentation in CosmosDB worker

---
+++ src/func-cosmosdb-worker/src/functions/xxENSONOxx.xxSTACKSxx.Worker/Program.cs
@@ -0,0 +1,57 @@
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using xxENSONOxx.xxSTACKSxx.Worker;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(context.HostingEnvironment.ContentRootPath)
+            .AddJsonFile("appsettings.json", false)
+            .AddEnvironmentVariables()
+            .Build();
+
+        services
+            .Configure<ChangeFeedListener>(configuration.GetSection(nameof(ChangeFeedListener)))
+            .AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.AddOpenTelemetry(options =>
+                {
+                    options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("xxENSONOxx.xxSTACKSxx.Listener"));
+                    options.IncludeFormattedMessage = true;
+                    options.IncludeScopes = true;
+                    options.AddConsoleExporter();
+                });
+            });
+            
+        services.AddOpenTelemetry().WithTracing(builder =>
+        {
+            builder.AddAspNetCoreInstrumentation()                      
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter();
+        });
+
+        services.AddOpenTelemetry().WithMetrics(builder =>
+        {
+            builder.AddAspNetCoreInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter();
+        });
+
+        services.AddOpenTelemetry().UseOtlpExporter();
+
+        services.AddOpenTelemetry().UseAzureMonitor();
+    })
+    .Build();
+
+await host.RunAsync();
