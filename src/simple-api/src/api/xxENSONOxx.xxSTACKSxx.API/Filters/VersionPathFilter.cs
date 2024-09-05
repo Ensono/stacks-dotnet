01e73155ba29937dc497ab95ed46aaec1ef0627b
Sharon Russell <sharon-russell@outlook.com>
2024-09-05 15:16:45 +0100

    refactor: 7707 Remove swagger api project (#483)
    * refactor: Remove swagger api project
    
    * refactor: remove unused packages
    
    * refactor: Remove unused packages and update solution file

---
+++ src/simple-api/src/api/xxENSONOxx.xxSTACKSxx.API/Filters/VersionPathFilter.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace xxENSONOxx.xxSTACKSxx.API.Filters;
+
+/// <summary>
+/// Filter operation based on path versioning
+/// </summary>
+public class VersionPathFilter(string basePath) : IDocumentFilter
+{
+    public string BasePath { get; } = basePath;
+
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        if (!BasePath.Contains(swaggerDoc.Info.Version))
+            return;
+
+        var pathsToModify = swaggerDoc.Paths.ToList();
+        foreach (var path in pathsToModify)
+        {
+            if (!path.Key.StartsWith(BasePath))
+            {
+                swaggerDoc.Paths.Remove(path.Key);
+            }
+        }
+    }
+}
--- src/simple-api/src/api/xxENSONOxx.xxSTACKSxx.API/Program.cs
+++ src/simple-api/src/api/xxENSONOxx.xxSTACKSxx.API/Program.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using xxENSONOxx.xxSTACKSxx.Shared.API.Middleware;
-using xxENSONOxx.xxSTACKSxx.Shared.API.Swagger.Filters;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +17,7 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 using xxENSONOxx.xxSTACKSxx.API;
 using xxENSONOxx.xxSTACKSxx.API.Authentication;
 using xxENSONOxx.xxSTACKSxx.API.Authorization;
+using xxENSONOxx.xxSTACKSxx.API.Filters;
 using xxENSONOxx.xxSTACKSxx.API.Models.Requests;
 
 var builder = WebApplication.CreateBuilder(args);
