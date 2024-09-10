43ee0b67af4f1dbd6f246f328bfbf094057150fd
Sharon Russell <sharon.russell@ensono.com>
2024-09-10 11:45:31 +0100

    refactor: update the tests and exclude parts we don't care about

--- src/simple-api/src/api/xxENSONOxx.xxSTACKSxx.API.UnitTests/ConfigurableAuthorizationPolicyProviderTests.cs
+++ src/simple-api/src/api/xxENSONOxx.xxSTACKSxx.API.UnitTests/ConfigurableAuthorizationPolicyProviderTests.cs
@@ -1,6 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using Xunit;
+using xxENSONOxx.xxSTACKSxx.API.Authentication;
+using xxENSONOxx.xxSTACKSxx.API.Authorization;
+
 namespace xxENSONOxx.xxSTACKSxx.API.UnitTests;
 
 public class ConfigurableAuthorizationPolicyProviderTests
 {
-    
+    [Fact]
+    public async Task GetDefaultPolicyAsync_ReturnsDefaultPolicy_WhenJwtBearerAuthenticationIsEnabled()
+    {
+        var options = Options.Create(new AuthorizationOptions());
+        var jwtOptions = Options.Create(new JwtBearerAuthenticationConfigurationExtension { Enabled = false });
+        var provider = new ConfigurableAuthorizationPolicyProvider(options, jwtOptions);
+
+        var result = await provider.GetDefaultPolicyAsync();
+
+        Assert.NotNull(result);
+        Assert.NotEmpty(result.Requirements);
+    }
 }
