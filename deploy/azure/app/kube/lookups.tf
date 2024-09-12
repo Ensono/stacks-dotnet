fcfcb145390759395a3fe73651347e03b1ad00b4
Saul France <saul.france@ensono.com>
2024-09-12 14:33:12 +0100

    feat: cosmosdb worker pipeline (#506)
    * feat: add cosmosdb worker pipeline
    
    * fix: linting
    
    * fix: linting
    
    * fix: update dockerfile
    
    * feat: add dev infrastructure
    
    * fix: update terraform
    
    * fix: set service bus name
    
    * fix: linting
    
    * fix: service bus lookup
    
    * fix: service bus lookup
    
    * fix: use linux function app resource
    
    * fix: upgrade provider and module versions
    
    * fix: update default dotnet version
    
    * fix: add cosmosdb lookup
    
    * chore: uncomment infra apply
    
    * fix: create resource group
    
    * fix: generate storage account name
    
    * fix: change app service plan tier
    
    * feat: add dev deploy
    
    * fix: update template path
    
    * fix: linting
    
    * fix: remove test download step
    
    * fix: update server url for legacy function deployment
    
    * fix: change dns to alias
    
    * fix: linting
    
    * fix: core tf state
    
    * fix: update all pipelines for dns alias
    
    * chore: update template files
    
    * chore: set function resource group variable
    
    * fix: exclude vars from linting
    
    * chore: update existing templates
    
    * fix: add prod variables
    
    * fix: use resource group from app output
    
    * fix: update prod function name
    
    * chore: uncomment prod condition
    
    * chore: update template files

--- deploy/azure/app/kube/lookups.tf
+++ deploy/azure/app/kube/lookups.tf
@@ -15,3 +15,15 @@ data "azurerm_application_insights" "example" {
   name                = data.terraform_remote_state.core.outputs.app_insights_name
   resource_group_name = data.terraform_remote_state.core.outputs.resource_group_name
 }
+
+data "azurerm_servicebus_namespace" "sb" {
+  count               = var.create_function_app ? 1 : 0
+  name                = var.sb_name
+  resource_group_name = var.sb_resource_group_name
+}
+
+data "azurerm_cosmosdb_account" "cosmosdb" {
+  count               = !var.create_cosmosdb && var.create_function_app ? 1 : 0
+  name                = var.cosmosdb_account_name
+  resource_group_name = var.cosmosdb_resource_group_name
+}
--- deploy/azure/app/kube/main.tf
+++ deploy/azure/app/kube/main.tf
@@ -15,7 +15,7 @@ module "default_label" {
 }
 
 module "app" {
-  source                               = "git::https://github.com/ensono/stacks-terraform//azurerm/modules/azurerm-server-side-app?ref=v1.5.1"
+  source                               = "git::https://github.com/ensono/stacks-terraform//azurerm/modules/azurerm-server-side-app?ref=v6.0.17"
   create_cosmosdb                      = var.create_cosmosdb
   resource_namer                       = module.default_label.id
   resource_tags                        = module.default_label.tags
@@ -28,11 +28,12 @@ module "app" {
   create_dns_record                    = var.create_dns_record
   dns_record                           = var.dns_record
   dns_zone_name                        = data.terraform_remote_state.core.outputs.dns_base_domain
-  core_resource_group                  = data.terraform_remote_state.core.outputs.resource_group_name
   dns_zone_resource_group              = data.terraform_remote_state.core.outputs.dns_resource_group_name != "" ? data.terraform_remote_state.core.outputs.dns_resource_group_name : data.terraform_remote_state.core.outputs.resource_group_name
-  dns_a_records                        = [data.terraform_remote_state.core.outputs.app_gateway_ip]
+  dns_ip_address_name                  = var.public_ip_name
+  dns_ip_address_resource_group        = var.core_resource_group
   subscription_id                      = data.azurerm_client_config.current.subscription_id
   create_cdn_endpoint                  = var.create_cdn_endpoint
+  force_create_resource_group          = var.create_resource_group
   # Alternatively if you want you can pass in the IP directly and remove the need for a lookup
   # dns_a_records                        = ["0.1.23.45"]
 }
@@ -40,14 +41,17 @@ module "app" {
 module "servicebus" {
   count                   = contains(split(",", var.app_bus_type), "servicebus") ? 1 : 0
   source                  = "../servicebus"
-  resource_group_name     = module.default_label.id
+  resource_group_name     = var.sb_resource_group_name != "" ? var.sb_resource_group_name : module.app.resource_group
   resource_group_location = var.resource_group_location
+  sb_name                 = var.sb_name
+  sb_topic_name           = var.sb_topic_name
+  sb_subscription_name    = var.sb_subscription_name
 }
 
 module "eventhub" {
   count                   = contains(split(",", var.app_bus_type), "eventhub") ? 1 : 0
   source                  = "../eventhub"
-  resource_group_name     = module.default_label.id
+  resource_group_name     = module.app.resource_group
   resource_group_location = var.resource_group_location
 }
 
@@ -55,12 +59,12 @@ module "function" {
   count                        = var.create_function_app ? 1 : 0
   source                       = "../function"
   function_name                = var.function_name
-  resource_group_name          = module.default_label.id
+  resource_group_name          = module.app.resource_group
   resource_group_location      = var.resource_group_location
-  cosmosdb_database_name       = module.app.cosmosdb_database_name
+  cosmosdb_database_name       = var.create_cosmosdb ? module.app.cosmosdb_database_name : var.cosmosdb_account_name
   cosmosdb_collection_name     = var.cosmosdb_sql_container
-  cosmosdb_connection_string   = "AccountEndpoint=${module.app.cosmosdb_endpoint};AccountKey=${module.app.cosmosdb_primary_master_key};"
-  sb_topic_name                = module.servicebus[0].servicebus_topic_name
-  sb_subscription_name         = module.servicebus[0].servicebus_subscription_name
-  servicebus_connection_string = module.servicebus[0].servicebus_connectionstring
+  cosmosdb_connection_string   = var.create_cosmosdb ? "AccountEndpoint=${module.app.cosmosdb_endpoint};AccountKey=${module.app.cosmosdb_primary_master_key};" : "AccountEndpoint=${data.azurerm_cosmosdb_account.cosmosdb[0].endpoint};AccountKey=${data.azurerm_cosmosdb_account.cosmosdb[0].primary_key};"
+  sb_topic_name                = contains(split(",", var.app_bus_type), "servicebus") ? module.servicebus[0].servicebus_topic_name : var.sb_topic_name
+  sb_subscription_name         = contains(split(",", var.app_bus_type), "servicebus") ? module.servicebus[0].servicebus_subscription_name : var.sb_subscription_name
+  servicebus_connection_string = contains(split(",", var.app_bus_type), "servicebus") ? module.servicebus[0].servicebus_connectionstring : data.azurerm_servicebus_namespace.sb[0].default_primary_connection_string
 }
