8eab1a8aaca9c015791f5b454ed23b4ca2da961d
Saul France <saul.france@ensono.com>
2024-09-13 09:20:38 +0100

    fix: locals file

---
+++ deploy/azure/app/kube/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  lookup_cosmosdb_account    = !var.create_cosmosdb && var.create_function_app && var.cosmosdb_account_name != null && var.cosmosdb_resource_group_name != null
+  cosmosdb_connection_string = local.lookup_cosmosdb_account ? "AccountEndpoint=${data.azurerm_cosmosdb_account.cosmosdb[0].endpoint};AccountKey=${data.azurerm_cosmosdb_account.cosmosdb[0].primary_key};" : "AccountEndpoint=${module.app.cosmosdb_endpoint};AccountKey=${module.app.cosmosdb_primary_master_key};"
+}
