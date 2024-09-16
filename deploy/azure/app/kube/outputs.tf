64412b13dc14ca2fe0a4de3a5dc2253a196baacc
Saul France <saul.france@ensono.com>
2024-09-16 10:14:14 +0100

    fix: comment out outputs

--- deploy/azure/app/kube/outputs.tf
+++ deploy/azure/app/kube/outputs.tf
@@ -99,13 +99,13 @@ output "eventhub_sa_container" {
   value = contains(split(",", var.app_bus_type), "eventhub") ? module.eventhub.*.eventhub_sa_container[0] : null
 }
 
-output "eventhub_function_listener_name" {
-  value = contains(split(",", var.app_bus_type), "eventhub") ? module.eventhub.*.function_listener_name[0] : null
-}
+# output "eventhub_function_listener_name" {
+#   value = contains(split(",", var.app_bus_type), "eventhub") ? module.eventhub.*.function_listener_name[0] : null
+# }
 
-output "eventhub_function_listener_id" {
-  value = contains(split(",", var.app_bus_type), "eventhub") ? module.eventhub.*.function_listener_id[0] : null
-}
+# output "eventhub_function_listener_id" {
+#   value = contains(split(",", var.app_bus_type), "eventhub") ? module.eventhub.*.function_listener_id[0] : null
+# }
 
 # Core State Query Outputs (to reduce variable duplication)
 output "dns_base_domain" {
