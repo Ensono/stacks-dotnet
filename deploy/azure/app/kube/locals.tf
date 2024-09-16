3f8e630c98b092d1ca4835284c7f00c1c9634ead
Saul France <saul.france@ensono.com>
2024-09-16 14:32:04 +0100

    fix: update service bus lookup condition

--- deploy/azure/app/kube/locals.tf
+++ deploy/azure/app/kube/locals.tf
@@ -2,6 +2,7 @@ locals {
   lookup_cosmosdb_account     = !var.create_cosmosdb && var.create_function_app && var.cosmosdb_account_name != "" && var.cosmosdb_resource_group_name != ""
   cosmosdb_account_name       = var.create_cosmosdb ? module.app.cosmosdb_database_name : var.cosmosdb_account_name
   cosmosdb_connection_string  = local.lookup_cosmosdb_account ? "AccountEndpoint=${data.azurerm_cosmosdb_account.cosmosdb[0].endpoint};AccountKey=${data.azurerm_cosmosdb_account.cosmosdb[0].primary_key};" : "AccountEndpoint=${module.app.cosmosdb_endpoint};AccountKey=${module.app.cosmosdb_primary_master_key};"
+  lookup_servicebus           = var.create_function_app && var.sb_resource_group_name != "" && !contains(split(",", var.app_bus_type), "servicebus")
   lookup_eventhub             = var.create_function_app && var.eventhub_namespace != "" && var.eventhub_resource_group_name != "" && var.eventhub_sa_name != "" && !contains(split(",", var.app_bus_type), "eventhub")
   eventhub_connection_string  = local.lookup_eventhub ? data.azurerm_eventhub_namespace.eventhub_namespace[0].default_primary_connection_string : (contains(split(",", var.app_bus_type), "eventhub") ? module.eventhub[0].eventhub_connectionstring : null)
   eventhub_name               = local.lookup_eventhub ? var.eventhub_name : (contains(split(",", var.app_bus_type), "eventhub") ? module.eventhub[0].eventhub_name : null)
