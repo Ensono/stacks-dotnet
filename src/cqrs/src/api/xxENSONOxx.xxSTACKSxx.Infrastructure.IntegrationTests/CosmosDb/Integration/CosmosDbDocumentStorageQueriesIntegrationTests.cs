eb5bebe8efaf9d0ca50ecd59309adeea54e1bee1
Sharon Russell <sharon.russell@ensono.com>
2024-09-19 15:29:51 +0100

    refactor: fix another issue with usings

--- src/cqrs/src/api/xxENSONOxx.xxSTACKSxx.Infrastructure.IntegrationTests/CosmosDb/Integration/CosmosDbDocumentStorageQueriesIntegrationTests.cs
+++ src/cqrs/src/api/xxENSONOxx.xxSTACKSxx.Infrastructure.IntegrationTests/CosmosDb/Integration/CosmosDbDocumentStorageQueriesIntegrationTests.cs
@@ -12,6 +12,7 @@ using Xunit.Abstractions;
 using xxENSONOxx.xxSTACKSxx.Infrastructure.Abstractions;
 using xxENSONOxx.xxSTACKSxx.Infrastructure.Configuration;
 using xxENSONOxx.xxSTACKSxx.Infrastructure.IntegrationTests.CosmosDb.DataModel;
+using xxENSONOxx.xxSTACKSxx.Infrastructure.Secrets;
 
 namespace xxENSONOxx.xxSTACKSxx.Infrastructure.IntegrationTests.CosmosDb.Integration;
 
