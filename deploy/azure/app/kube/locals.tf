398c910cd777f560e8d5017795bfd78595d4a024
Saul France <saul.france@ensono.com>
2024-09-23 11:09:31 +0100

    feat: add background worker pipeline (#526)
    * chore: update helm to support workers
    
    * feat: background worker pipeline
    
    * chore: update simple api
    
    * fix: linting
    
    * chore: add create dns record variable
    
    * chore: temp comment out unit tests
    
    * fix: temp remove tests
    
    * fix: account for missing app bus type
    
    * fix: lookup servicebus information
    
    * chore: update all pipelines
    
    * fix: set required outputs
    
    * fix: set create resource group variable
    
    * fix: servicebus default values
    
    * 8074 - Add place holder unit test project in background worker
    
    * chore: reintroduce unit tests to pipeline
    
    * chore: update templates
    
    * 8074 - Update coverlet to use msbuild instead of collector and update versions of xunit
    
    * chore: json indentation
    
    * chore: json indentation
    
    * fix: enable ingress for aws
    
    * fix: background worker unit tests
    
    * fix: background worker unit tests
    
    * chore: json indentation
    
    ---------
    
    Co-authored-by: Ali Russell <awjrussell@live.co.uk>

--- deploy/azure/app/kube/locals.tf
+++ deploy/azure/app/kube/locals.tf
@@ -4,7 +4,7 @@ locals {
   cosmosdb_account_name      = var.create_cosmosdb ? module.app.cosmosdb_database_name : var.cosmosdb_account_name
   cosmosdb_connection_string = local.lookup_cosmosdb_account ? "AccountEndpoint=${data.azurerm_cosmosdb_account.cosmosdb[0].endpoint};AccountKey=${data.azurerm_cosmosdb_account.cosmosdb[0].primary_key};" : "AccountEndpoint=${module.app.cosmosdb_endpoint};AccountKey=${module.app.cosmosdb_primary_master_key};"
   # Service Bus
-  lookup_servicebus    = var.create_function_app && var.sb_resource_group_name != "" && !contains(split(",", var.app_bus_type), "servicebus")
+  lookup_servicebus    = var.sb_resource_group_name != "" && !contains(split(",", var.app_bus_type), "servicebus")
   sb_topic_id          = local.lookup_servicebus ? data.azurerm_servicebus_topic.sb_topic[0].id : (contains(split(",", var.app_bus_type), "servicebus") ? module.servicebus[0].servicebus_topic_id : null)
   sb_topic_name        = local.lookup_servicebus ? var.sb_topic_name : (contains(split(",", var.app_bus_type), "servicebus") ? module.servicebus[0].servicebus_topic_name : null)
   sb_connection_string = local.lookup_servicebus ? data.azurerm_servicebus_namespace.sb[0].default_primary_connection_string : (contains(split(",", var.app_bus_type), "servicebus") ? module.servicebus[0].servicebus_connectionstring : null)
---
+++ src/background-worker/src/worker/xxENSONOxx.xxSTACKSxx.BackgroundWorker.UnitTests/PlaceHolderTests.cs
@@ -0,0 +1,13 @@
+namespace xxENSONOxx.xxSTACKSxx.BackgroundWorker.UnitTests;
+
+public class PlaceHolderTests
+{
+    [Fact]
+    public void ReplaceMe()
+    {
+        var answer = 42;
+        var sum = 21 + 21;
+        
+        Assert.Equal(sum, answer);
+    }
+}
