9e941837f40db116bb9730c28ad2893eec5f2491
Andy Hale <Andrew.Hale@Ensono.com>
2024-09-25 14:59:02 +0100

    Create component tests.

---
+++ src/func-cosmosdb-worker/src/functions/xxENSONOxx.xxSTACKSxx.Worker.ComponentTests/Doubles/TestLogger.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Logging;
+
+namespace xxENSONOxx.xxSTACKSxx.Worker.ComponentTests.Doubles;
+
+public class TestLogger<T> : ILogger<T>
+{
+    public List<string> LogMessages { get; } = [];
+
+    public IDisposable BeginScope<TState>(TState state) => null;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        LogMessages.Add(formatter(state, exception));
+    }
+}
---
+++ src/func-cosmosdb-worker/src/functions/xxENSONOxx.xxSTACKSxx.Worker.ComponentTests/Fixtures/ChangeFeedListenerFixture.cs
@@ -0,0 +1,99 @@
+using xxENSONOxx.xxSTACKSxx.Application.CQRS.Events;
+using xxENSONOxx.xxSTACKSxx.Application.CQRS.Events.Abstractions.ApplicationEvents;
+using xxENSONOxx.xxSTACKSxx.Worker.ComponentTests.Doubles;
+
+namespace xxENSONOxx.xxSTACKSxx.Worker.ComponentTests.Fixtures;
+
+public class ChangeFeedListenerFixture
+{
+    private const string DocumentsChangesMessage = "Documents modified";
+    private const string DocumentReadMessage = "Document read. Id:";
+    
+    private readonly Random _random;
+    private readonly IFixture _autoFixture;
+    private readonly IApplicationEventPublisher _applicationEventPublisher;
+    private readonly TestLogger<ChangeFeedListener> _logger;
+    private readonly ChangeFeedListener _systemUnderTest;
+
+    private int _changeFeedEventCount;
+    private List<CosmosDbChangeFeedEvent> _changeFeedEvents;
+
+    public ChangeFeedListenerFixture()
+    {
+        _random = new Random();
+        _autoFixture = new Fixture().Customize(new AutoNSubstituteCustomization());
+        _applicationEventPublisher = Substitute.For<IApplicationEventPublisher>();
+        _logger = new TestLogger<ChangeFeedListener>();
+        _systemUnderTest = new ChangeFeedListener(_applicationEventPublisher, _logger);
+        _changeFeedEventCount = 0;
+        _changeFeedEvents = [];
+    }
+
+    //
+    //  Given
+    //
+
+    public void GivenCosmosDbTriggerReceivesEvents()
+    {
+        _changeFeedEventCount = _random.Next(1, 11); 
+        _changeFeedEvents = _autoFixture.CreateMany<CosmosDbChangeFeedEvent>(_changeFeedEventCount).ToList();
+    }
+
+    public void GivenCosmosDbTriggerReceivesNoEvents()
+    {
+        _changeFeedEvents = [];
+        _changeFeedEventCount = _changeFeedEvents.Count;
+    }
+
+    //
+    //  When
+    //
+
+    public void WhenFunctionIsTriggered()
+    {
+        Action act = () => _systemUnderTest.Run(_changeFeedEvents);
+        act.Should().NotThrow();
+    }
+
+    //
+    // Then
+    //
+
+    public void ThenApplicationEventsArePublished()
+    {
+        _applicationEventPublisher.Received(_changeFeedEventCount).PublishAsync(Arg.Any<IApplicationEvent>());
+    }
+
+
+    public void ThenNoApplicationEventsArePublished()
+    {
+        _applicationEventPublisher.DidNotReceive().PublishAsync(Arg.Any<CosmosDbChangeFeedEvent>());
+    }
+
+
+    public void ThenLogWrittenToShowEventsReceived()
+    {
+        _logger.LogMessages.Should().Contain($"{DocumentsChangesMessage} {_changeFeedEventCount}");
+    }
+
+
+    public void ThenNoLogWrittenToShowEventsReceived()
+    {
+        _logger.LogMessages.Should().NotContain(DocumentsChangesMessage);
+    }
+
+
+    public void ThenLogWrittenWithEachEventEntityId()
+    {
+        foreach (var document in _changeFeedEvents)
+        {
+            _logger.LogMessages.Should().Contain($"{DocumentReadMessage} {document.EntityId}");
+        }
+    }
+
+
+    public void ThenNoLogsWrittenWithEventEntityIds()
+    {
+        _logger.LogMessages.Should().NotContain(DocumentReadMessage);
+    }
+}
