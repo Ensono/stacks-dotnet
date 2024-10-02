2c24578e81daa92e4d4649e5c275cdc970486661
Andy Hale <Andrew.Hale@Ensono.com>
2024-10-02 10:03:45 +0100

    Adjust events and handlers to use the interfaces in the shared library.

--- src/background-worker/src/worker/xxENSONOxx.xxSTACKSxx.BackgroundWorker/Handlers/CategoryDeletedEventHandler.cs
+++ src/background-worker/src/worker/xxENSONOxx.xxSTACKSxx.BackgroundWorker/Handlers/CategoryDeletedEventHandler.cs
@@ -1,7 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using xxENSONOxx.xxSTACKSxx.Application.CQRS.Events;
-using xxENSONOxx.xxSTACKSxx.Application.CQRS.Events.Abstractions.ApplicationEvents;
+using xxENSONOxx.xxSTACKSxx.Shared.Messaging.Azure.ServiceBus.Abstractions.ApplicationEvents;
 
 namespace xxENSONOxx.xxSTACKSxx.BackgroundWorker.Handlers;
 
