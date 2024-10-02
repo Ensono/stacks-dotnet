e0b37b602c4abc4c358f898d35629c5dadc0f194
Andy Hale <Andrew.Hale@Ensono.com>
2024-10-02 10:21:49 +0100

    Adjust operations context to use the interfaces in the shared library.

--- src/background-worker/src/worker/xxENSONOxx.xxSTACKSxx.Application.CQRS.Events/Abstractions/ApplicationEvents/IApplicationEvent.cs
+++ src/background-worker/src/worker/xxENSONOxx.xxSTACKSxx.Application.CQRS.Events/Abstractions/ApplicationEvents/IApplicationEvent.cs
@@ -1,4 +1,4 @@
-using xxENSONOxx.xxSTACKSxx.Application.CQRS.Events.Operations;
+using xxENSONOxx.xxSTACKSxx.Shared.Messaging.Azure.ServiceBus.Operations;
 
 namespace xxENSONOxx.xxSTACKSxx.Application.CQRS.Events.Abstractions.ApplicationEvents;
 
