801511152dd62fa032b194d62148eb9712f7e5bb
Andy Hale <Andrew.Hale@Ensono.com>
2024-10-08 14:34:20 +0100

    Compute the Enclosed Message Type.

--- src/background-worker/src/tests/Functional/xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests/Tests/Steps/MenuCreatedSteps.cs
+++ src/background-worker/src/tests/Functional/xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests/Tests/Steps/MenuCreatedSteps.cs
@@ -3,12 +3,11 @@ using Newtonsoft.Json;
 using xxENSONOxx.xxSTACKSxx.BackgroundWorker.ApplicationEvents.Events;
 using xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests.Configuration;
 using xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests.Drivers;
-using xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests.Drivers.ServiceBus;
 using xxENSONOxx.xxSTACKSxx.Shared.Messaging.Azure.ServiceBus.Serializers;
 
 namespace xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests.Tests.Steps;
 
-public class ServiceBusSteps
+public class MenuCreatedSteps
 {
     private readonly ServiceBusDriver _serviceBusDriver;
     private IReadOnlyList<ServiceBusReceivedMessage>? _activeMessageList;
@@ -19,15 +18,16 @@ public class ServiceBusSteps
     private ServiceBusMessage _serviceBusMessage;
     private ServiceBusReceivedMessage? _serviceBusReceivedMessage;
     private StacksCloudEvent<MenuCreatedEvent> _expectedEvent;
+    private readonly string _eventsNamespace;
 
     private readonly AsyncPolicy<bool> _topicExistsRetryPolicy;
     private readonly AsyncPolicy<ServiceBusReceivedMessage?> _serviceBusRetryPolicy;
 
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ServiceBusSteps"/> class.
+    /// Initializes a new instance of the <see cref="MenuCreatedSteps"/> class.
     /// </summary>
-    public ServiceBusSteps()
+    public MenuCreatedSteps()
     {
         var config = ConfigurationAccessor.GetApplicationConfiguration();
         var testDrivers = new TestDrivers();
@@ -35,8 +35,11 @@ public class ServiceBusSteps
         _subscriptionName = config.SubscriptionName;
         _serviceBusDriver = testDrivers.ServiceBusDriver;
         _topicExistsRetryPolicy = ServiceBusDriver.GetTopicExistsRetryPolicy();
-        _serviceBusRetryPolicy = ServiceBusDriver.GetServiceBusRetryPolicy();
+        _serviceBusRetryPolicy = ServiceBusDriver.GetMessageRetryPolicy();
+        _eventsNamespace = "xxENSONOxx.xxSTACKSxx.BackgroundWorker";
+
     }
+
 
     /// <summary>
     /// Clear all messages from a topic and subscription.
@@ -51,6 +54,7 @@ public class ServiceBusSteps
         );
     }
 
+
     /// <summary>
     /// Check that a Service Bus Topic exists.
     /// </summary>
@@ -58,8 +62,9 @@ public class ServiceBusSteps
     public async Task CheckThatTopicExists()
     {
         var topicExits = await _serviceBusDriver.CheckTopicExistsAsync(_topicName);
-        topicExits.ShouldBeTrue();
+        topicExits.ShouldBeTrue("The Service Bus Topic does not exist.");
     }
+
 
     /// <summary>
     /// Add a valid event to the Service Bus Topic.
@@ -67,11 +72,11 @@ public class ServiceBusSteps
     /// <returns></returns>
     public async Task AddAValidEventToServiceBus()
     {
-        var eventMessageBody = GetData<StacksCloudEvent<MenuCreatedEvent>>("Data/MenuCreatedEvent.json");
-        //var eventMessageType = typeof(StacksCloudEvent<MenuCreatedEvent>);
-        var enclosedMessageType = "xxENSONOxx.xxSTACKSxx.Shared.Messaging.Azure.ServiceBus.Serializers.StacksCloudEvent`1[[xxENSONOxx.xxSTACKSxx.BackgroundWorker.ApplicationEvents.Events.MenuCreatedEvent, xxENSONOxx.xxSTACKSxx.BackgroundWorker]], xxENSONOxx.xxSTACKSxx.Shared.Messaging.Azure.ServiceBus";
-
+        var eventMessageBody = GetDataFromFile<StacksCloudEvent<MenuCreatedEvent>>("Data/MenuCreatedEvent.json");
         _expectedEvent = JsonConvert.DeserializeObject<StacksCloudEvent<MenuCreatedEvent>>(eventMessageBody)!;
+
+        var enclosedMessageType = GetEnclosedMessageType(_expectedEvent);
+
         _serviceBusMessage = new ServiceBusMessage(eventMessageBody)
         {
             ContentType = "application/json",
@@ -82,21 +87,28 @@ public class ServiceBusSteps
     }
 
 
-    public async Task AddAInValidEventToServiceBus()
+    /// <summary>
+    /// Add a message with an invalid message type to the Service Bus Topic,
+    /// which will prevent deserialization.
+    /// </summary>
+    /// <returns></returns>
+    public async Task AddAInvalidEventToServiceBus()
     {
-        var eventMessageBody = GetData<StacksCloudEvent<MenuCreatedEvent>>("Data/MenuCreatedEvent.json");
+        var eventMessageBody = GetDataFromFile<StacksCloudEvent<MenuCreatedEvent>>("Data/MenuCreatedEvent.json");
+        var eventMessageType = "INVALID-MESSAGE-TYPE";
         _expectedEvent = JsonConvert.DeserializeObject<StacksCloudEvent<MenuCreatedEvent>>(eventMessageBody)!;
         _serviceBusMessage = new ServiceBusMessage(eventMessageBody)
         {
             ContentType = "application/json",
-            ApplicationProperties = { ["EnclosedMessageType"] = "INCORREC-MESSAGE-TYPE" }
+            ApplicationProperties = { ["EnclosedMessageType"] = eventMessageType }
         };
         await _serviceBusDriver.AddMessageToTopic(_topicName, _serviceBusMessage);
     }
 
 
     /// <summary>
-    /// Check, with retry policy, that the worker has processed the message.
+    /// Check, with retry policy, that the worker has read and processed the message, therefore
+    /// removing it from the topic.
     /// </summary>
     /// <returns></returns>
     public async Task ConfirmEventHasBeenProcessedByWorker()
@@ -112,15 +124,16 @@ public class ServiceBusSteps
                     ReceiveMode = ServiceBusReceiveMode.PeekLock
                 });
 
-            return _activeMessageList.Count < 1;
+            return _activeMessageList is { Count: < 1 };
         });
 
         _activeMessageList.ShouldBeEmpty("Message has not been processed by the Worker.");
     }
 
+
     /// <summary>
-    /// Check, with retry policy, that the message is not on the Dead letter queue
-    /// to prevent a false positive.
+    /// Check that the message is not on the Dead letter queue.  A retry policy is used
+    /// to ensure that the dead letter message isn't delayed to prevent a false positive.
     /// </summary>
     /// <returns></returns>
     public async Task ConfirmEventIsNotPresentInDeadLetter()
@@ -135,21 +148,23 @@ public class ServiceBusSteps
                     SubQueue = SubQueue.DeadLetter,
                     ReceiveMode = ServiceBusReceiveMode.PeekLock
                 });
+
             _serviceBusReceivedMessage = _deadLetterMessageList.FirstOrDefault(x => x.Body.ToString().Contains(_expectedEvent.Id))!;
 
             return _serviceBusReceivedMessage is not null;
         });
 
-        _serviceBusReceivedMessage = _deadLetterMessageList.FirstOrDefault(x => x.Body.ToString().Contains(_expectedEvent.Id!))!;
-        _serviceBusReceivedMessage.ShouldBeNull($"Valid message is in Deadletter queue for TransactionID {_expectedEvent.Id}");
+        _serviceBusReceivedMessage = _deadLetterMessageList.FirstOrDefault(x => x.Body.ToString().Contains(_expectedEvent.Id))!;
+        _serviceBusReceivedMessage.ShouldBeNull($"Valid message is in the Dead Letter queue for TransactionID {_expectedEvent.Id}");
     }
+
 
     /// <summary>
     /// Fetch the message from DeadLetter with retry
     /// </summary>
     /// <returns></returns>
     /// <exception cref="Exception"></exception>
-    public async Task ConfirmPaymentIsPresentInDeadLetter()
+    public async Task ConfirmEventIsPresentInDeadLetter()
     {
         _serviceBusReceivedMessage = await _serviceBusRetryPolicy.ExecuteAsync(async () =>
         {
@@ -163,8 +178,7 @@ public class ServiceBusSteps
                 });
 
             // Checks for Message in DeadLetter Queue
-            var message = _deadLetterMessageList
-                .FirstOrDefault(x => x.Body.ToString().Contains(_expectedEvent.Id!));
+            var message = _deadLetterMessageList.FirstOrDefault(x => x.Body.ToString().Contains(_expectedEvent.Id!));
             return message;
         });
 
@@ -184,7 +198,20 @@ public class ServiceBusSteps
         });
     }
 
-    private static string GetData<T>(string dataFilePath)
+
+    private  string GetEnclosedMessageType<T>(StacksCloudEvent<T> message) where T : class
+    {
+        Type messageType = message.GetType();
+        var cloudEventTypeName = messageType.FullName[..messageType.FullName.IndexOf('`')];
+
+        var cloudEventDataType = messageType.GetGenericArguments()[0];
+        var cloudEventDataTypeName = cloudEventDataType.FullName;
+
+        return $"{cloudEventTypeName}`1[[{cloudEventDataTypeName}, {_eventsNamespace}]]";
+    }
+
+
+    private static string GetDataFromFile<T>(string dataFilePath)
     {
         var data = File.ReadAllText(dataFilePath);
         var expectedModel = JsonConvert.DeserializeObject<T>(data);
