630685709fc288898c7adc42eb4b0d0c1c9be5ae
Andy Hale <AndyHale@users.noreply.github.com>
2024-10-10 15:59:49 +0100

    Fix:  CosmosDbChangeFeedEvent deserialization error. (#572)
    * CosmosDB Azure Function:  Try System.Text.Json for event deserialization.
    * CosmosDB Azure Function:  Remove Newtonsoft.Json.

--- src/func-cosmosdb-worker/src/functions/xxENSONOxx.xxSTACKSxx.Worker/CosmosDbChangeFeedEvent.cs
+++ src/func-cosmosdb-worker/src/functions/xxENSONOxx.xxSTACKSxx.Worker/CosmosDbChangeFeedEvent.cs
@@ -1,4 +1,4 @@
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 using xxENSONOxx.xxSTACKSxx.Shared.Messaging.Azure.ServiceBus.Abstractions.ApplicationEvents;
 using xxENSONOxx.xxSTACKSxx.Shared.Messaging.Azure.ServiceBus.Operations;
 
