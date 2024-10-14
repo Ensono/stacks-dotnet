99d02ed116db8eec1bff47424803540029ebafe2
Andy Hale <AndyHale@users.noreply.github.com>
2024-10-14 11:54:08 +0100

    [7757] Add Functional Tests to Service Bus Worker (#568)
    * Create a functional tests project for the Service Bus Worker.
    * Enable functional tests in pipeleine.
    * Set environment for functional tests from Terraform output.
    * Update packages.

---
+++ src/background-worker/src/tests/Functional/xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests/Tests/Stories/MenuCreatedStory.cs
@@ -0,0 +1,41 @@
+using xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests.Tests.Steps;
+
+namespace xxENSONOxx.xxSTACKSxx.BackgroundWorker.FunctionalTests.Tests.Stories;
+
+[Story(
+    AsA = "Kitchen stock manager",
+    IWant = "to know when a new menu is created",
+    SoThat = "I can place an order for new ingredients")]
+public class MenuCreatedStory : IAsyncLifetime
+{
+    private readonly MenuCreatedSteps _menuCreatedSteps = new();
+
+    public async Task InitializeAsync()
+    {
+        await _menuCreatedSteps.ClearTopicAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public void Confirm_valid_new_menu_event_is_processed_by_worker_service()
+    {
+        this.Given(s => _menuCreatedSteps.CheckThatTopicExists(), "Given an existing Azure Service Bus Topic")
+             .When(s => _menuCreatedSteps.AddAValidEventToServiceBus(), "When a valid 'Menu Created' event is sent to the Service Bus")
+             .Then(s => _menuCreatedSteps.ConfirmEventHasBeenProcessedByWorker(), "The event is processed by the Worker")
+              .And(s => _menuCreatedSteps.ConfirmEventIsNotPresentInDeadLetter(), "The event is not sent to dead-letter.")
+            .BDDfy();
+    }
+
+    [Fact]
+    public void Confirm_invalid_new_menu_event_is_not_processed_by_worker_service()
+    {
+        this.Given(s => _menuCreatedSteps.CheckThatTopicExists(), "Given an existing Azure Service Bus Topic")
+             .When(s => _menuCreatedSteps.AddAInvalidEventToServiceBus(), "When an invalid 'Menu Created' event is sent to the Service Bus")
+             .Then(s => _menuCreatedSteps.ConfirmEventIsPresentInDeadLetter(), "The event is sent to dead-letter.")
+            .BDDfy();
+    }
+}
