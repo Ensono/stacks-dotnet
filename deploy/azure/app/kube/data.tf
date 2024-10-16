5343b00871d0f961be0b883bf5408637a9070785
Bal Purewal <baldip.purewal@ensono.com>
2024-10-16 14:43:55 +0100

    remove build

--- deploy/azure/app/kube/data.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_container_registry" "acr" {
-  name                = "ensonoeuw"
-  resource_group_name = "stacks-ancillary-resources"
-}
-
-data "azurerm_container_app_environment" "acae" {
-  name                = "ed-stacks-nonprod-uks-aca"
-  resource_group_name = "ed-stacks-nonprod-uks-aca"
-}
