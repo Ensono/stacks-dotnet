683c2720b1c2eddda7c3dd160e7c8b329625feb4
Saul France <saul.france@ensono.com>
2024-10-16 10:58:07 +0100

    fix: outputs

--- deploy/azure/app/servicebus/outputs.tf
+++ deploy/azure/app/servicebus/outputs.tf
@@ -1,19 +1,19 @@
 output "servicebus_namespace" {
   description = "Service bus namespace"
-  value       = azurerm_servicebus_namespace.sb[0].name
+  value       = var.create_sb_namespace ? azurerm_servicebus_namespace.sb[0].name : null
 }
 
 output "servicebus_topic_name" {
   description = "Name of the topic"
-  value       = azurerm_servicebus_topic.sb_topic[0].name
+  value       = var.create_sb_topic ? azurerm_servicebus_topic.sb_topic[0].name : null
 }
 
 output "servicebus_topic_id" {
   description = "ID of the topic"
-  value       = azurerm_servicebus_topic.sb_topic[0].id
+  value       = var.create_sb_topic ? azurerm_servicebus_topic.sb_topic[0].id : null
 }
 
 output "servicebus_connectionstring" {
-  value     = azurerm_servicebus_namespace.sb[0].default_primary_connection_string
+  value     = var.create_sb_namespace ? azurerm_servicebus_namespace.sb[0].default_primary_connection_string : null
   sensitive = true
 }
