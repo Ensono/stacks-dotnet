893c3c4af078a9613cefea654574b99f21c48ef3
Saul France <saul.france@ensono.com>
2024-10-18 14:38:09 +0100

    fix: update environment variables

--- build/deployment_list.ps1
+++ build/deployment_list.ps1
@@ -31,6 +31,18 @@
       servicebus_connectionstring = "`${SERVICEBUS_CONNECTIONSTRING}"
     }
   }
+  @{
+    displayName = "FunctionalTests"
+    template = "tests/xxENSONOxx.xxSTACKSxx.Worker.FunctionalTests/appsettings.json"
+    vars = @{
+      cosmosdb_connection_string = "`${COSMOSDB_CONNECTION_STRING}"
+      cosmosdb_name = "`${COSMOSDB_DATABASE_NAME}"
+      cosmosdb_container_name = "`${COSMOSDB_CONTAINER_NAME}"
+      servicebus_topic_name = "`${SERVICEBUS_TOPIC_NAME}"
+      servicebus_subscription_name = "`${SERVICEBUS_SUBSCRIPTION_NAME}"
+      servicebus_connectionstring = "`${SERVICEBUS_CONNECTIONSTRING}"
+    }
+  }
 )
 
 # # AWS is expecting env vars or TF outputs for:
