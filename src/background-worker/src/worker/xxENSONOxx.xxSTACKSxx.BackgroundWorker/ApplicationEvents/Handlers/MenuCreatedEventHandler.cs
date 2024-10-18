6b7c368d0d1cb306483cbaac55d9b8c04938a18f
Andy Hale <Andrew.Hale@Ensono.com>
2024-10-18 10:15:15 +0100

    Revert: Temporary debugging adjustments.

--- src/background-worker/src/worker/xxENSONOxx.xxSTACKSxx.BackgroundWorker/ApplicationEvents/Handlers/MenuCreatedEventHandler.cs
+++ src/background-worker/src/worker/xxENSONOxx.xxSTACKSxx.BackgroundWorker/ApplicationEvents/Handlers/MenuCreatedEventHandler.cs
@@ -3,14 +3,11 @@ using xxENSONOxx.xxSTACKSxx.Shared.Messaging.Azure.ServiceBus.Abstractions.Appli
 
 namespace xxENSONOxx.xxSTACKSxx.BackgroundWorker.ApplicationEvents.Handlers;
 
-public class MenuCreatedEventHandler(ILogger<MenuCreatedEventHandler> logger, IConfiguration configuration)
+public class MenuCreatedEventHandler(ILogger<MenuCreatedEventHandler> logger)
            : IApplicationEventHandler<MenuCreatedEvent>
 {
     public Task HandleAsync(MenuCreatedEvent appEvent)
     {
-        string topicName = configuration["ServiceBusConfiguration:Listener:Topics:0:Name"];
-        string subscriptionName = configuration["ServiceBusConfiguration:Listener:Topics:0:SubscriptionName"];
-        logger.LogInformation($"Using Topic {topicName} and Subscription {subscriptionName}");
         logger.LogInformation("Executing MenuCreatedEventHandler...");
         return Task.CompletedTask;
     }
