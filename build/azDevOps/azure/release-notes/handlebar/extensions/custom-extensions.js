ea7169c9bc01c7902d1c67bae13b08f55e90ac69
Daniel Phillips <38911348+Danphillipz@users.noreply.github.com>
2024-10-21 15:12:32 +0100

    8345 - Automated Release Note Generation  (#585)
    * Update release note handlebar template and pipeline template. Remove release not publishing from NuGet pipeline
    
    * Add optional release note step to the CI-CQRS pipeline. Release note flag added to vars file
    
    * YML formatting
    
    * YML formatting
    
    * YML formatting
    
    * Enable release note generation
    
    * Update release note template path
    
    * Reference wiki in ci-cqrs pipeline
    
    * Trial release notes as a stage
    
    * Add flag to vars template. Add output vars job to determine environment for release notes
    
    * Switch from stageDependencies to dependencies
    
    * Retry stage dependencies
    
    * Remove release note step from Dev stage. Change release note path
    
    * Move output vars into deploy jobs
    
    * Attempt different syntax for getting variable from Deployment job
    
    * Dump Json for development
    
    * Create stage template for release note generation
    
    * Migrate all logic to release-note-stage.yml
    
    * Add release notes to all pipelines
    
    * Switch to adding content to start of each minor version page
    
    * Add minor V
    
    * Update template to cater for GitHub and add parseJson extension
    
    * Add missing version header
    
    * Add TOC section for each build
    
    * Add TOC section for each build
    
    * Add missing pipe
    
    * Action PR feedback. Moved release notes out of ensono folder so that they're included in templates.
    Added conditions as parameters for the release note job
    Make repo input for the publish release note task dynamic
    Updated all templates with steps for release notes
    
    * Update release note stage to use the parameters
    
    * Update expected trees
    
    * Revert "Update expected trees"
    
    This reverts commit 4cbaf90fac1f23455d2a54c187c0c49a6c0c8ee0.
    
    * update expected trees
    
    ---------
    
    Co-authored-by: Saul France <saul.france@ensono.com>

---
+++ build/azDevOps/azure/release-notes/handlebar/extensions/custom-extensions.js
@@ -0,0 +1,49 @@
+function getTestRunUrl(apiUrl, runId) {
+    const urlParts = apiUrl.split('/');
+    const baseUrl = urlParts.slice(0, -2).join('/').replace('_apis/test', '_TestManagement/Runs');
+    return `${baseUrl}?runId=${runId}&_a=runCharts`;
+}
+
+function generateTestSummary(testResults) {
+    const testRuns = {};
+    testResults.forEach(result => {
+        const testRunId = result.testRun.id;
+        if (!testRuns[testRunId]) {
+            testRuns[testRunId] = {
+                name: result.testRun.name,
+                totalTests: 0,
+                passedTests: 0,
+                url: getTestRunUrl(result.testRun.url, testRunId)
+            };
+        }
+        testRuns[testRunId].totalTests += 1;
+        if (result.outcome === "Passed") {
+            testRuns[testRunId].passedTests += 1;
+        }
+    });
+
+    // Create the summary array
+    const summary = Object.keys(testRuns).map(testRunId => {
+        const totalTests = testRuns[testRunId].totalTests;
+        const passedTests = testRuns[testRunId].passedTests;
+        const passRate = `${passedTests}/${totalTests} ${(passedTests / totalTests * 100).toFixed(2)}%`;
+        return {
+            name: testRuns[testRunId].name,
+            testRunId: testRunId,
+            totalTests: totalTests,
+            passRate: passRate,
+            url: testRuns[testRunId].url
+        };
+    });
+
+    return summary;
+}
+
+function parseJson(json) {
+  return JSON.parse(jsonString);
+}
+
+module.exports = {
+    generateTestSummary,
+    parseJson
+};
