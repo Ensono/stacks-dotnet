db0a68dead89ea0eaf1f680a26585c2750f34575
Bal Purewal <baldip.purewal@ensono.com>
2024-10-22 13:41:03 +0100

    set variables for functional test

--- deploy/azure/app/aca/outputs.tf
+++ deploy/azure/app/aca/outputs.tf
@@ -20,3 +20,19 @@ output "container_app_principal_id" {
   description = "The Principal ID of the Container App's Managed Identity"
   value       = module.aca.container_app_principal_id
 }
+
+# Output for functional tests
+output "servicebus_topic_name" {
+  description = "Name of the topic"
+  value       = var.servicebus_type != null ? data.terraform_remote_state.app.outputs.servicebus_topic_name : null
+}
+
+output "servicebus_connectionstring" {
+  value     = var.servicebus_type != null ? data.terraform_remote_state.app.outputs.servicebus_connectionstring : null
+  sensitive = true
+}
+
+output "servicebus_subscription_name" {
+  description = "Servicebus Subscription name"
+  value       = var.servicebus_type == "listener" ? data.terraform_remote_state.app.outputs.servicebus_subscription_name : null
+}
