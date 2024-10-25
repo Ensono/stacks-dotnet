7ab2c8e81f91eef53555043acbfadd9aacab97ad
Andy Hale <AndyHale@users.noreply.github.com>
2024-10-25 12:50:54 +0100

    7908 - Functional Tests for the Cosmosdb Worker Function (#581)
    * Create Functions Tests project based on previous example.
    * fix: install taskctl
    * fix: update environment variables
    * fix: linting
    * fix: infra init
    * chore: update all pipeline files
    * chore: split templater files
    * fix: templater file path
    * chore: functional test env var
    * chore: add templater file to all pipelines
    * fix: move functional tests to separate step
    * update pipeline so its consistent with other pipelines where functional tests are a seperate task
    * added functional test step
    * seperate functional tests step for other aca pipelines
    * added TF_FILE_INFRA_LOCATION
    * update prod namespace
    * added ENV_NAME
    * updated expected trees
    updated readme with deployment mode for ACA
    
    ---------
    
    Co-authored-by: Federica Colapaoli <fcolapaoli@andplus.com>
    Co-authored-by: Jack Middleton <jack.middleton@kingfisher.com>
    Co-authored-by: Ali Russell <awjrussell@live.co.uk>
    Co-authored-by: Saul France <saul.france@ensono.com>
    Co-authored-by: Sharon Russell <sharon-russell@outlook.com>
    Co-authored-by: Bal Purewal <baldip.purewal@ensono.com>

---
+++ src/func-cosmosdb-worker/src/tests/Functional/xxENSONOxx.xxSTACKSxx.Worker.FunctionalTests/Drivers/ServiceBusDriver.cs
@@ -0,0 +1,168 @@
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+using Microsoft.Extensions.DependencyInjection;
+using xxENSONOxx.xxSTACKSxx.Worker.FunctionalTests.Models;
+
+namespace xxENSONOxx.xxSTACKSxx.Worker.FunctionalTests.Drivers;
+
+public class ServiceBusDriver
+{
+    private readonly ServiceBusAdministrationClient _administrationClient;
+    private readonly ServiceBusClient _serviceBusClient;
+    private readonly AsyncPolicy<bool> _retryTopicExistsPolicy;
+    private readonly AsyncPolicy<ServiceBusReceivedMessage?> _retryReceiveMessagePolicy;
+
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceBusDriver"/> class.
+    /// </summary>
+    /// <param name="clientConnectionString">The service bus connection string</param>
+    /// <param name="adminConnectionString">The service bus administration client connection string</param>
+    public ServiceBusDriver(string clientConnectionString, string adminConnectionString)
+    {
+        var serviceCollection = new ServiceCollection()
+            .AddTransient(_ => new ServiceBusClient(clientConnectionString))
+            .AddTransient(_ => new ServiceBusAdministrationClient(adminConnectionString));
+
+        IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+        _serviceBusClient = serviceProvider.GetRequiredService<ServiceBusClient>();
+        _administrationClient = serviceProvider.GetRequiredService<ServiceBusAdministrationClient>();
+        _retryTopicExistsPolicy = GetRetryTopicExistsPolicy();
+        _retryReceiveMessagePolicy = GetRetryReceiveMessageIfNullPolicy();
+    }
+
+
+    /// <summary>
+    /// Check if given topic exits.
+    /// </summary>
+    /// <param name="topicPath">Name of the topic</param>
+    /// <returns>A <see cref="Task"/> Returns boolean value.</returns>
+    public async Task<bool> CheckTopicExistsAsync(string topicPath)
+    {
+        try
+        {
+            return await _retryTopicExistsPolicy.ExecuteAsync(async () =>
+            {
+                var result = await _administrationClient.TopicExistsAsync(topicPath);
+                return result.HasValue && result.Value;
+            });
+        }
+        catch (Exception ex)
+        {
+            return Task.FromException(ex).IsFaulted;
+        }
+    }
+
+
+    /// <summary>
+    /// Reads and returns a list of messages from a given Topic.
+    /// </summary>
+    /// <param name="topicName"> sets topic name</param>
+    /// <param name="subscriptionName">sets subscriptions name </param>
+    /// <param name="options"> sets processorOptions</param>
+    /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+    public async Task<IReadOnlyList<ServiceBusReceivedMessage>?> ReadMessagesAsync(string topicName, string subscriptionName, ServiceBusReceiverOptions options)
+    {
+        var receiver = _serviceBusClient.CreateReceiver(topicName, subscriptionName, options);
+        try
+        {
+            return await receiver.ReceiveMessagesAsync(10, TimeSpan.FromSeconds(5));
+        }
+        catch (AggregateException ex)
+        {
+            Console.WriteLine(ex.InnerException?.Message ?? "An AggregateException was thrown.");
+            return null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.InnerException?.Message ?? "An Exception was thrown.");
+            return null;
+        }
+        finally
+        {
+            await receiver.DisposeAsync();
+        }
+    }
+
+
+    /// <summary>
+    /// Reads a given topic and subscription for a message containing the given CosmosChangeFeedEvent.
+    /// </summary>
+    /// <param name="topicName">The topic to search for the given message.</param>
+    /// <param name="subscriptionName">The subscription to search for the given message.</param>
+    /// <param name="subQueue">The sub-queue to search for the given message.</param>
+    /// <param name="cosmosDbChangeFeedEvent">The message to search for</param>
+    /// <returns></returns>
+    public async Task<ServiceBusReceivedMessage?> ConfirmMessagePresentInQueueAsync(string topicName, string subscriptionName, SubQueue subQueue, CosmosDbChangeFeedEvent cosmosDbChangeFeedEvent)
+    {
+        return await _retryReceiveMessagePolicy.ExecuteAsync(async () =>
+        {
+            var messageList = await this.ReadMessagesAsync(
+                topicName,
+                subscriptionName,
+                new ServiceBusReceiverOptions { SubQueue = subQueue, ReceiveMode = ServiceBusReceiveMode.PeekLock });
+
+            var message = messageList?.FirstOrDefault(x => x.Body.ToString().Contains(cosmosDbChangeFeedEvent.CorrelationId!));
+            return message;
+        });
+    }
+
+
+    /// <summary>
+    /// Clears all messages from a topic and subscription.
+    /// </summary>
+    /// <param name="topicName">The topic to clear.</param>
+    /// <param name="subscriptionName">The subscription to clear.</param>
+    /// <param name="subQueue">The sub-queue to clear.</param>
+    /// <returns></returns>
+    public async Task ClearTopicAsync(string topicName, string subscriptionName, SubQueue subQueue)
+    {
+        var receiver = _serviceBusClient.CreateReceiver(
+            topicName,
+            subscriptionName,
+            new ServiceBusReceiverOptions
+            {
+                ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete,
+                SubQueue = subQueue
+            });
+        while (await receiver.PeekMessageAsync() != null)
+        {
+            await receiver.ReceiveMessagesAsync(100);
+        }
+
+        await receiver.CloseAsync();
+    }
+
+
+    /// <summary>
+    /// Returns a Retry Policy used when checking if a message was received.
+    /// </summary>
+    public static AsyncPolicy<ServiceBusReceivedMessage?> GetRetryReceiveMessageIfNullPolicy() =>
+        Policy
+            .HandleResult<ServiceBusReceivedMessage?>(b => b == null)
+            .WaitAndRetryAsync(
+                retryCount: 5,
+                sleepDurationProvider: _ => TimeSpan.FromSeconds(10),
+                onRetry: (_, timeSpan, retryCount, _) =>
+                {
+                    Console.WriteLine($"Retry receiving Service Bus message.  Retry count:  {retryCount}.  " +
+                                      $"Waiting {timeSpan} before next retry.");
+                }
+            );
+
+    /// <summary>
+    /// Returns a Retry Policy used when checking if a topic exists.
+    /// </summary>
+    public static AsyncPolicy<bool> GetRetryTopicExistsPolicy() =>
+        Policy
+            .HandleResult<bool>(b => !b)
+            .WaitAndRetryAsync(
+                retryCount: 5,
+                sleepDurationProvider: _ => TimeSpan.FromSeconds(10),
+                onRetry: (_, timeSpan, retryCount, _) =>
+                {
+                    Console.WriteLine($"Retry checking if Topic exists.  Retry count:  {retryCount}. " +
+                                      $"Waiting {timeSpan} before next retry.");
+                }
+            );
+}
---
+++ src/func-cosmosdb-worker/src/tests/Functional/xxENSONOxx.xxSTACKSxx.Worker.FunctionalTests/Tests/Stories/ExampleStory.cs
@@ -0,0 +1,61 @@
+using xxENSONOxx.xxSTACKSxx.Worker.FunctionalTests.Tests.Steps;
+
+namespace xxENSONOxx.xxSTACKSxx.Worker.FunctionalTests.Tests.Stories;
+
+[Story(
+    AsA = "A Stacks User",
+    IWant = "to know when a change has been made in Cosmos",
+    SoThat = "my downstream functions can react to the change")]
+
+public class ExampleStory : IAsyncLifetime
+{
+    private readonly ExampleSteps _exampleSteps = new();
+
+
+    public async Task InitializeAsync()
+    {
+        await _exampleSteps.ClearTopicAsync();
+        await _exampleSteps.DeleteItemsCreatedInCosmosDb();
+    }
+
+
+    public async Task DisposeAsync()
+    {
+        await _exampleSteps.DeleteItemsCreatedInCosmosDb();
+    }
+
+
+    [Fact]
+    public void Confirm_when_create_valid_item_in_CosmosDb_then_event_is_published_to_Service_Bus()
+    {
+        this.Given(step => _exampleSteps.ConfirmCosmosDbContainerExists(), "the Cosmos DB container exists")
+              .And(step => _exampleSteps.ConfirmServiceBusTopicExists(), "the Azure Service Bus Topic exists")
+             .When(step => _exampleSteps.CreateValidItemInCosmosDbContainer(), "an item with valid data is created in the CosmosDB container")
+             .Then(step => _exampleSteps.ConfirmServiceBusMessageForItemCreatedIsReceived(), "a message is published to the Service Bus Topic")
+            .BDDfy();
+    }
+
+
+    [Fact]
+    public void Confirm_when_attempt_to_create_invalid_item_in_CosmosDb_then_event_is_not_published_to_Service_Bus()
+    {
+        this.Given(step => _exampleSteps.ConfirmCosmosDbContainerExists(), "the Cosmos DB container exists")
+              .And(step => _exampleSteps.ConfirmServiceBusTopicExists(), "the Azure Service Bus Topic exists")
+             .When(step => _exampleSteps.CreateInvalidItemInCosmosDbContainer(), "an item with invalid data is created in the CosmosDB container")
+             .Then(step => _exampleSteps.ConfirmServiceBusMessageForItemCreatedIsNotReceived(), "a message is not published to the Service Bus Topic")
+              .And(step => _exampleSteps.ConfirmServiceBusMessageForItemCreatedIsNotMovedToDeadLetter(), "the message has not been moved to Dead Letters")
+            .BDDfy();
+    }
+
+
+    [Fact]
+    public void Confirm_when_update_item_in_CosmosDb_then_event_is_published_to_Service_Bus()
+    {
+        this.Given(step => _exampleSteps.ConfirmCosmosDbContainerExists(), "the Cosmos DB container exists")
+              .And(step => _exampleSteps.ConfirmServiceBusTopicExists(), "the Azure Service Bus Topic exists")
+              .And(step => _exampleSteps.CreateValidItemInCosmosDbContainer(), "an item has already been created in the container")
+             .When(step => _exampleSteps.UpdateItemInCosmosDbContainer(), "the item that was created in the CosmosDB container is updated")
+             .Then(step => _exampleSteps.ConfirmServiceBusMessageForItemUpdatedIsReceived(), "a message is published to the Service Bus Topic")
+            .BDDfy();
+    }
+}
